70da44866e47c34f4ef339f552a82d9003135cea
Aleksey Kuksov <aleksey.kuksov@gmail.com>
2025-03-26 14:30:01 +0300

    new version

--- app.py
+++ app.py
@@ -1,17 +1,46 @@
 from typing import Any
-from flask import Flask, jsonify, request
 from random import choice
-from pathlib import Path
 import sqlite3
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, jsonify, abort, request
+from pathlib import Path
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import String
 
-
+class Base(DeclarativeBase):
+    pass
 
 BASE_DIR = Path(__file__).parent
 path_to_db = BASE_DIR / "store.db" # <- тут путь к БД
 
 app = Flask(__name__)
 app.json.ensure_ascii = False
+app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{BASE_DIR / 'main.db'}"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
+
+class QuoteModel(db.Model):
+    __tablename__ = 'quotes'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    author: Mapped[str] = mapped_column(String(32), unique=False, index=True)
+    text: Mapped[str] = mapped_column(String(255))
+    rating: Mapped[int]# = mapped_column(default=1)
+
+    def __init__(self, author, text, rating):
+        self.author = author
+        self.text = text
+        self.rating = rating
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "author": self.author,
+            "text": self.text,
+            "rating": self.rating
+        }
 
 # quotes = [
 #     {
@@ -49,19 +78,10 @@ app.json.ensure_ascii = False
 @app.route("/quotes")
 def get_quotes() -> list[dict[str, Any]]:
     """Функция неявно преобразовывает список словарей в JSON"""
-    select_quotes = "SELECT * from quotes"
-    connection = sqlite3.connect("store.db")
-    cursor = connection.cursor()
-    cursor.execute(select_quotes)
-    quotes_db = cursor.fetchall()
-    cursor.close()
-    connection.close()
-    
-    keys = ("id", "author", "text")
-    quotes= []
-    for quote_db in quotes_db:
-        quote = dict(zip(keys, quote_db))
-        quotes.append(quote)
+    quotes_db = db.session.scalars(db.select(QuoteModel)).all()
+    quotes = []
+    for quote in quotes_db:
+        quotes.append(quote.to_dict())
     return jsonify(quotes), 200
     
 
@@ -69,18 +89,10 @@ def get_quotes() -> list[dict[str, Any]]:
 
 
 @app.route("/quotes/<int:quote_id>")
-def get_quote(quote_id: int) -> dict:
-    select_quote = "SELECT * FROM quotes WHERE id = ?"
-    connection = sqlite3.connect("store.db")
-    cursor = connection.cursor()
-    quote_db = cursor.execute(select_quote, (quote_id,)).fetchone()
-    cursor.close()
-    connection.close()
-    if quote_db:
-        keys = ("id", "author", "text")
-        quote = dict(zip(keys, quote_db))
-        return jsonify(quote), 200
-    return f"Quote with id={quote_id} not found", 404
+def get_quote(quote_id: int):
+    select_quote = db.get_or_404(QuoteModel, quote_id, description=f"Quote with id={quote_id} not found")
+    quote = select_quote.to_dict()
+    return jsonify(quote), 200
 
 
 # 3
@@ -102,48 +114,26 @@ def get_quote(quote_id: int) -> dict:
 @app.route("/quotes", methods=['POST'])
 def create_quote():
     new_quote = request.json
-    insert_quote = "INSERT INTO quotes (author, text) VALUES (?, ?)"
-    connection = sqlite3.connect("store.db")
-    cursor = connection.cursor()
-    cursor.execute(insert_quote, (new_quote['author'], new_quote['text']))
-    new_quote_db = cursor.lastrowid
-    connection.commit()
-    cursor.close()
-    connection.close()
-    new_quote['id'] = new_quote_db
+    quote = QuoteModel(**new_quote)
+    db.session.add(quote)
+    db.session.commit()
     return jsonify(new_quote), 201
 
 @app.route("/quotes/<int:quote_id>", methods=["DELETE"])
 def delete_quote(quote_id: int):
-    delete_sql = f"DELETE FROM quotes WHERE id = ?"
-    params = (quote_id,)
-    connection = sqlite3.connect("store.db")
-    cursor = connection.cursor()
-    cursor.execute(delete_sql, params)
-    connection.commit()
-    rows = cursor.rowcount
-    cursor.close()
-    connection.close()
-    if rows:
-        return jsonify(f"Quote with {id=} is deleted."), 200
-    return f"Quote with id={quote_id} not found", 404
+    select_quote = db.get_or_404(QuoteModel, quote_id, description=f"Quote with id={quote_id} not found")
+    db.session.delete(select_quote)
+    db.session.commit()
+    return jsonify({"message": f"Quote with id={quote_id} has deleted"}), 200
 
 @app.route("/quotes/<int:quote_id>", methods=["PUT"])
 def edit_quote(quote_id):
+    select_quote = db.get_or_404(QuoteModel, quote_id, description=f"Quote with id={quote_id} not found")
     new_data = request.json
-    attributes: set = set(new_data.keys()) & {'author', 'text'}
-    update_quote = f"UPDATE quotes SET {', '.join(attr + '=?' for attr in attributes)} WHERE id = ?"
-    params = tuple(new_data.get(attr) for attr in attributes) + (quote_id,)
-    connection = sqlite3.connect("store.db")
-    cursor = connection.cursor()
-    cursor.execute(update_quote, params)
-    connection.commit()
-    rows = cursor.rowcount
-    cursor.close()
-    connection.close()
-    if rows:
-        return jsonify({"message": f"Quote with id={quote_id} has updated"}), 200
-    return f"Quote with id={quote_id} not found", 404
+    for key, value in new_data.items():
+        setattr(select_quote, key, value)
+    db.session.commit()    
+    return jsonify({"message": f"Quote with id={quote_id} has updated"}), 200
 
 if __name__ == "__main__":
     app.run(debug=True)
